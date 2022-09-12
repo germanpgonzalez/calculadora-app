4d66f1b0bdb3c63e01736b88c4ab6fce0a11af3b
germancai <germancai@hotmail.com>
2022-09-12 09:09:51 -0300

    Calculadora creada en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora-app';
+
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  titulo: String = 'Aplicación de Calculadora'
+
+  sumar():void {
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+
+  reiniciar():void {
+    this.resultado = 0;
+    this.numero1 = 0;
+    this.numero2 = 0;
+  }
+
 }
